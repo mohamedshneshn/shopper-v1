1b50eb5f9a13beb0f20d6fe6bd0406affa59c96b
mohamedshneshn <eng.mohamed.shneshn@gmail.com>
2024-04-26 18:03:39 -0700

    first commit

---
+++ intro.js
@@ -0,0 +1,35 @@
+/*
+fron-end
+--------
+1-install dependencies
+--------------------
+npm install react-router-dom
+
+
+2-remove unwanted files & clean up App.js
+---------------------
+App.css
+App.test.js
+logo.svg
+setupTests.js
+
+3- adjust title in index.html
+
+4- create components folder
+
+5- create folder structure
+---------------------
+Components
+    -Navbar
+       -Navbar.js
+       -Navbar.css
+Context
+Pages
+Assets
+
+
+
+
+
+
+*/
