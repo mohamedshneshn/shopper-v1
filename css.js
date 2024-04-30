c982d2050b246447fa72c80d07dc8bbd41657de8
mohamedshneshn <eng.mohamed.shneshn@gmail.com>
2024-04-30 00:24:59 -0700

    fdfsd

---
+++ css.js
@@ -0,0 +1,36 @@
+/*
+display Flex
+----------------
+display: flex;   // default is row
+
+flex-direction: row | row-reverse | column | column-reverse;  //used to set the direction of the main axis
+
+flex-wrap: nowrap | wrap | wrap-reverse;  //used to set the direction of the cross axis
+
+flex-flow: <flex-direction> || <flex-wrap>;  //used to set both the direction of the main axis and the cross axis
+
+justify-content: flex-start | flex-end | center | space-between | space-around;  //used to align the flex items along the main axis
+
+align-items: flex-start | flex-end | center | baseline | stretch;  //used to align the flex items along the cross axis
+
+align-content: flex-start | flex-end | center | space-between | space-around | stretch;  //used to align the flex lines along the cross axis
+
+order: <integer>;  //used to set the order of the flex items
+
+flex-grow: <number>;  //used to specify how much the item will grow relative to the rest
+
+flex-shrink: <number>;  //used to specify how much the item will shrink relative to the rest
+
+flex-basis: <length> | auto;  //used to specify the initial main size of the flex item
+
+flex: none | [ <'flex-grow'> <'flex-shrink'>? || <'flex-basis'> ];  //used to specify the components of a flex item
+
+align-self: auto | flex-start | flex-end | center | baseline | stretch;  //used to align the flex item along the cross axis
+
+
+
+
+
+
+
+*/
